1e5211dad3b9f27ac70421c4233b785d927d748f
annewaithaka <annewaithaka68@gmail.com>
2024-07-01 08:51:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(driver) {
+    return driver.slice(0, 2); 
+    // create a new array with the first two elements using slice
+}
+
+function returnLastTwoDrivers(driver) {
+    return driver.slice(-2); 
+    // Use slice with negative index to get the last two elements
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+      return fare * multiplier;
+    };
+  }
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+  }
